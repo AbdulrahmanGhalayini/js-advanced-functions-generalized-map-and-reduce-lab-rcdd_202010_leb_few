c655db1538338c64885739c494293f604a36ef1d
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-31 17:11:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,53 +1,21 @@
-<<<<<<< HEAD
-=======
+function map(src, cb) {
+  let r = []
 
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
 
-function mapToNegativize(s){
-  return s.map(x => x*-1);
+  return r;
 }
 
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
 
-function mapToNoChange(s){
-  return s.map(x => x);
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
 }
-
-function mapToDouble(s){
-  return s.map(x => x*2);
-}
-
-function mapToSquare(s){
-  return s.map(x => x*x);
-}
-
-
-
-function reduceToTotal(s , a=0){
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
-  return s.reduce(reducer , a);
-}
-
-
-function reduceToAllTrue(s ){
-
-  return s.reduce((acc, val) => {
-  return val = val ? true : false;
-}); 
-}
-
-
-function reduceToAnyTrue(s ){
-
-  return s.reduce((acc, val) => {
-    console.log("acc= "+ acc+ " val= "+ val);
-  return val || acc ? true : false;
-}); 
-}
-
-
-
-function reduceToTotal(s , a=){
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  return s.reduce(reducer , a);
-}
->>>>>>> e5b752208e6355ff7c31720dfc69e2b328af0bda
